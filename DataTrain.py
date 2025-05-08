8eec162711df9271579acbe5c7f4ae6c06e20500
OthmaneBk <okebennani@gmail.com>
2025-05-09 00:03:54 +0100

    dataTrain

---
+++ DataTrain.py
@@ -0,0 +1,25 @@
+description_list = [
+    "Chemise en coton à motifs floraux",
+    "Ordinateur portable avec processeur Intel Core i7",
+    "Pantalon en jean stretch pour hommes",
+    "Téléviseur 4K avec écran OLED",
+    "Chaussures de sport pour la course",
+    "Smartphone avec écran tactile et triple caméra",
+    "Veste en cuir pour femmes",
+    "Casque audio sans fil avec réduction de bruit",
+    "Sandales en cuir pour femmes",
+    "Tablette graphique avec stylet"
+]
+
+labels_list = [
+    "Vêtements",
+    "Électronique",
+    "Vêtements",
+    "Électronique",
+    "Chaussures",
+    "Électronique",
+    "Vêtements",
+    "Électronique",
+    "Chaussures",
+    "Électronique"
+]
