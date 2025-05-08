d0ecc596d64dc62b14b6421a5849a4d3bf48edd5
OthmaneBk <okebennani@gmail.com>
2025-05-09 00:03:45 +0100

    dataset

---
+++ DataSet.py
@@ -0,0 +1,4 @@
+descriptifs_test = ["Ensemble Costume 3 pièces, tissu en polyester de qualité premium",
+ "Ordinateur PC de dernière génération dotée d'une caméra LiDar et d'une RAM performante",
+ "Sneaker noir adapté à la course à pied"
+    ]
